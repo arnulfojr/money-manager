9e9a37e071dcf4f0fb887abd12840d6f0705f288
Arnulfo Solis <arnulfojr94@gmail.com>
2017-09-10 18:06:31 +0200

    updated config to fit gunicorn

--- src/init.py
+++ src/init.py
@@ -7,6 +7,8 @@ from werkzeug.serving import run_simple
 from api import app as api_app
 from frontend import app as frontend_app
 
+from settings import USE_RELOADER, USE_DEBUGGER
+
 
 application = DispatcherMiddleware(frontend_app, {
     '/api': api_app
@@ -14,4 +16,4 @@ application = DispatcherMiddleware(frontend_app, {
 
 if __name__ == '__main__':
     run_simple('0.0.0.0', 5000, application,
-            use_reloader=True, use_debugger=True)
+            use_reloader=USE_RELOADER, use_debugger=USE_DEBUGGER)
