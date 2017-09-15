c458a7b52987163ef3724a86b8d185bf10ffd31f
Arnulfo Solis <arnulfojr94@gmail.com>
2017-09-15 23:28:31 +0200

    frontend has hello blueprint

--- src/frontend/__init__.py
+++ src/frontend/__init__.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 # import blueprints
 from hello import blueprint as hello_bp
 
+# register blueprints
 app.register_blueprint(hello_bp)
 
--- src/frontend/hello/controllers/hello.py
+++ src/frontend/hello/controllers/hello.py
@@ -1,13 +1,18 @@
 
+from flask import make_response
+
 from .. import blueprint
 
 
 @blueprint.route('/', methods=['GET'])
 def hello():
-    return u"Hello!"
+    return make_response(u"Hello", 200)
 
 
 @blueprint.route('/<name>/', methods=['GET'])
 def hello_world(name='World'):
-    return u"Hello {}!".format(name)
+    """Says hello world with the given name"""
+    message = u"Hello {}!".format(name)
+    response = make_response(message, 200)
+    return response
 
